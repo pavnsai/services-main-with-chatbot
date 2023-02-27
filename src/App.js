bc659cc560839c539d2c6b8d640f990fcb7677fd
P@ V aN <pavansaiappari@Pavans-MacBook-Air.local>
2023-02-27 21:44:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Cart from './components/Cart';
+import ServicePage from './components/ServicePage';
+import React from 'react';
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import awsconfig from './aws-exports';
+import { Amplify, Auth, API, graphqlOperation } from 'aws-amplify';
+import CheckOut from './components/CheckOut';
+import DummySignInPage from './components/DummySignInPage';
+import Orders from './components/Orders';
+import ChatbotButton from './ChatButton';
+
+Amplify.configure(awsconfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Header />
+            <div className="App">
+                <Route path="/" exact>
+                    <ServicePage />
+                </Route>
+                <Route path="/home" exact>
+                    <Home />
+                </Route>
+                <Route path="/cart">
+                    <Cart />
+                </Route>
+                <Route path="/checkout">
+                    <CheckOut />
+                </Route>
+                <Route path="/orders">
+                    <Orders />
+                </Route>
+                <Route path="/login">
+                    <DummySignInPage />
+                </Route>
+                {/* <ChatbotButton /> */}
+                <ChatbotButton />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
